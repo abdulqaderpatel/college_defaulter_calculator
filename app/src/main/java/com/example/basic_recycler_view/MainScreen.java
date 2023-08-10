f71dae5ccde0ffa507aa73222991bfbecdd0aae7
abdulqader <abdulqaderpatel2002@gmail.com>
2023-08-10 21:07:44 +0530

    Added home page UI

--- app/src/main/java/com/example/basic_recycler_view/MainScreen.java
+++ app/src/main/java/com/example/basic_recycler_view/MainScreen.java
@@ -6,15 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.basic_recycler_view.add_students.add_students;
 import com.example.basic_recycler_view.add_subjects.add_subjects;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainScreen extends AppCompatActivity {
     Button take,view;
-    LinearLayout take_attendance,check_attendance,add_subject,add_student;
+    RelativeLayout take_attendance,check_attendance;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +26,23 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         take_attendance=findViewById(R.id.take_attendance);
         check_attendance=findViewById(R.id.check_attendance);
-        add_student=findViewById(R.id.add_students);
-        add_subject=findViewById(R.id.add_subjects);
 
-        add_student.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), add_students.class);
-                startActivity(intent);
-            }
-        });
 
-        add_subject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), add_subjects.class);
-                startActivity(intent);
-            }
-        });
+//        add_student.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(getApplicationContext(), add_students.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        add_subject.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(getApplicationContext(), add_subjects.class);
+//                startActivity(intent);
+//            }
+//        });
 
         take_attendance.setOnClickListener(new View.OnClickListener() {
             @Override
